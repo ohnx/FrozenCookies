fb6d63c67d150ae25bf44e535f2f73a8ea9d3eac
Mason X <ohnx@users.noreply.github.com>
2016-05-06 21:37:12 -0400

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -1,16 +1,17 @@
 var FrozenCookies = {
-	'baseUrl': baseUrl,
 	'branch' : 'Σ',
 	'version': '1.9.3'
 };
 FrozenCookies.loadInterval = setInterval(function() {
   if (Game && Game.ready) {
+  	console.log("loaded!");
 document.addEventListener('keydown', function(event) {
   if (!Game.promptOn) {
     if(event.keyCode == 71) {
 Game.goldenCookie.time = Game.goldenCookie.minTime;
 Game.goldenCookie.life = 0;
 Game.goldenCookie.spawn();
+console.log("giving you a GC!");
     }
   }
 });
